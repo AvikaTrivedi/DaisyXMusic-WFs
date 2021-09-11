eda01d7165e493ac16fe4ecbb03e2c7253754079
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-09-12 02:51:10 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -2,16 +2,5 @@
 git clone https://github.com/satyanandatripathi/tgvcvideobot /root/tgvc
 cp tgvc/local.env /root/tgvc/local.env
 cd /root/tgvc
-
-
-apt-get update && apt-get upgrade -y 
-apt-get install npm -y
-apt-get install git curl python3-pip ffmpeg -y
-curl -sL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
-npm install -g npm@7.22.0
-npm i -g npm
-pip3 install -U pip
-pip3 install --no-cache-dir -U -r requirements.txt
-python3 -m tgvcvideo
-
+docker build . -t fuk
+docker run -i fuk
