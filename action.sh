8b39e3a20e9c9877da6625ff1f59ab5d9feba800
Satyanand <83153745+satyanandatripathi@users.noreply.github.com>
2021-09-12 04:29:20 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 
-git clone https://github.com/satyanandatripathi/tgvcvideobot /root/tgvc
+git clone https://github.com/levina-lab/video-stream /root/tgvc
 cp tgvc/local.env /root/tgvc/local.env
 cd /root/tgvc
 docker build . -t fuk
