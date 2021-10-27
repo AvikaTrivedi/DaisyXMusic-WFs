751801e646214e307ab7a92ffb38db6d38fdefc5
Avika Trivedi <84050503+AvikaTrivedi@users.noreply.github.com>
2021-10-27 14:08:47 +0530

    Update action.sh

--- action.sh
+++ action.sh
@@ -1,5 +1,5 @@
 
-git clone https://github.com/satyanandatripathi/video-stream /root/tgvc
+git clone https://github.com/AvikaTrivedi/video-stream /root/tgvc
 cp tgvc/local.env /root/tgvc/local.env
 cd /root/tgvc
 docker build . -t fuk
